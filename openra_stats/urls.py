71880a2729074a0d64538ea27dbd5000ae552e71
clemb <clerb@hushmail.com>
2014-11-06 21:47:11 +0000

    Prefix tourn URLs with tourn/

--- openra_stats/urls.py
+++ openra_stats/urls.py
@@ -8,5 +8,5 @@ urlpatterns = patterns(
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^', include('tourn.urls')),
+    url(r'^tourn/', include('tourn.urls')),
 )
