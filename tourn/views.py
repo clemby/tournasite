af11adde1cf37e1d5b4ba4ed317b34946c791841
clemb <clerb@hushmail.com>
2014-11-07 17:09:52 +0000

    Refactor using generic views

--- tourn/views.py
+++ tourn/views.py
@@ -1,56 +1,14 @@
-from django.http import Http404
+from rest_framework import generics
 
-from rest_framework import status
-from rest_framework.decorators import APIView
-from rest_framework.response import Response
-
-from tourn.models import Team
-from tourn.serializers import TeamSerializer
+from .models import Team
+from .serializers import TeamSerializer
 
 
-class TeamList(APIView):
-    """List all teams, or create a new one."""
-    def get(self, request, format=None):
-        teams = Team.objects.all()
-        serializer = TeamSerializer(teams, many=True)
-        return Response(serializer.data)
-
-    def post(self, request, format=None):
-        serializer = TeamSerializer(data=request.DATA)
-
-        if not serializer.is_valid():
-            return Response(serializer.errors,
-                            status=status.HTTP_400_BAD_REQUEST)
-
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
+class TeamList(generics.ListCreateAPIView):
+    queryset = Team.objects.all()
+    serializer_class = TeamSerializer
 
 
-class TeamDetail(APIView):
-    """Retrieve, update or delete a team."""
-    def get_object(self, pk):
-        try:
-            return Team.objects.get(pk=pk)
-        except Team.DoesNotExist:
-            raise Http404
-
-    def get(self, request, pk, format=None):
-        team = self.get_object(pk)
-        serializer = TeamSerializer(team)
-        return Response(serializer.data)
-
-    def put(self, request, pk, format=None):
-        team = self.get_object(pk)
-        serializer = TeamSerializer(team, data=request.DATA)
-
-        if not serializer.is_valid():
-            return Response(serializer.errors,
-                            status=status.HTTP_400_BAD_REQUEST)
-
-        serializer.save()
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def delete(self, request, pk, format=None):
-        team = self.get_object(pk)
-        team.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+class TeamDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Team.objects.all()
+    serializer_class = TeamSerializer
