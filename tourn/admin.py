c163df00a91a584ddc48377ba672d17e1ab5c395
clemb <clerb@hushmail.com>
2014-11-11 22:06:57 +0000

    Admin site tweaks

--- tourn/admin.py
+++ tourn/admin.py
@@ -3,8 +3,17 @@ from django.contrib import admin
 from . import models
 
 
-admin.site.register(models.Tournament)
-admin.site.register(models.Match)
+class MatchInline(admin.TabularInline):
+    model = models.Match
+    extra = 0
+
+
+class TournamentAdmin(admin.ModelAdmin):
+    inlines = (MatchInline,)
+    list_display = ('name', 'planned_start', 'planned_finish')
+
+
+admin.site.register(models.Tournament, TournamentAdmin)
 
 
 class TeamEntryInline(admin.TabularInline):
@@ -14,7 +23,7 @@ class TeamEntryInline(admin.TabularInline):
 
 class TeamAdmin(admin.ModelAdmin):
     fields = ('name', 'creator')
-    inlines = [TeamEntryInline]
+    inlines = (TeamEntryInline,)
     list_display = ('name', 'creator')
 
 
