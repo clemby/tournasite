d54e4e81fccd79526059c4146da529df4ebb37c2
clemb <clerb@hushmail.com>
2014-11-11 22:27:45 +0000

    Move API stuff into /api/
    Also change URL config: tournaments API is now
    reachable at /api/v1/tourn/

---
+++ api/urls.py
@@ -0,0 +1,18 @@
+from django.conf.urls import patterns, include, url
+
+from rest_framework.routers import DefaultRouter
+
+from . import views
+
+
+router = DefaultRouter()
+router.register(r'teams', views.TeamViewSet)
+router.register(r'players', views.PlayerViewSet)
+router.register(r'matches', views.MatchViewSet)
+router.register(r'tournaments', views.TournamentViewSet)
+
+
+urlpatterns = patterns(
+    '',
+    url(r'tourn/', include(router.urls)),
+)
--- openra_stats/urls.py
+++ openra_stats/urls.py
@@ -1,8 +1,6 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
-from tourn.urls import router as tourn_router
-
 
 urlpatterns = patterns(
     '',
@@ -11,7 +9,7 @@ urlpatterns = patterns(
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^tourn/', include(tourn_router.urls)),
+    url(r'^api/v1/', include('api.urls')),
     url(r'^api-auth/', include('rest_framework.urls',
                                namespace='rest_framework'))
 )
