927b5786d537e49315158e799efb6bd4def6604a
clemb <clerb@hushmail.com>
2014-11-18 07:04:27 +0000

    Move base.css to openra_stats/static

--- openra_stats/settings.py
+++ openra_stats/settings.py
@@ -96,6 +96,10 @@ TEMPLATE_DIRS = (
     os.path.join(BASE_DIR, 'registration/templates'),
 )
 
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'openra_stats/static'),
+)
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
