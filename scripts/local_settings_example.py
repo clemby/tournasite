d90fa2de3c63fd2d5697953f5c10c71a81177307
clemb <clerb@hushmail.com>
2014-11-27 15:54:58 +0000

    Add example (dev) local_settings.py

---
+++ scripts/local_settings_example.py
@@ -0,0 +1,16 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'db/database.db',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '',
+    }
+}
+
+
+INTERNAL_IPS = ('127.0.0.1',)
+
+
+DEBUG = True
