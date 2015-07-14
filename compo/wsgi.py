0cd7dda6a35308fa522f330f6c66c4d090a4e210
clemb <clerb@hushmail.com>
2015-07-14 18:47:08 +0100

    wsgi.py changes

--- compo/wsgi.py
+++ compo/wsgi.py
@@ -8,7 +8,18 @@ https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 """
 
 import os
+import logging
+import sys
+
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "compo.settings")
+
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+
+sys.path.append(BASE_DIR)
+os.chdir(BASE_DIR)
+
+logging.basicConfig(stream=sys.stderr)
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
